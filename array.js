57d793fcc6d1266d29ddc4cfd95a74001c617086
sthaRajan450 <stharajan477@gmail.com>
2025-09-21 09:33:26 +0545

    array

---
+++ array.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeroes = [];
+var heroPowers = [];
+superHeroes.push("superman");
+heroPowers.push(1);
+var allUsers = [];
+allUsers.push({
+    name: 'rajan',
+    isActive: true
+});
---
+++ array.ts
@@ -0,0 +1,16 @@
+const superHeroes:string[]= [];
+const heroPowers:Array <number>=[]
+superHeroes.push("superman");
+heroPowers.push(1)
+
+type User={
+    name:string,
+    isActive:boolean
+}
+
+const allUsers:User[]=[]
+allUsers.push({
+    name:'rajan',
+    isActive:true
+})
+export {}
