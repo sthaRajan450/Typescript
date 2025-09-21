87f11ccb4aa8df8e3d04019f8aec35b04d70eec1
sthaRajan450 <stharajan477@gmail.com>
2025-09-21 13:29:11 +0545

    tuple in ts

---
+++ tuple.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// const user:(string|number)[]=[1,'hc',3,4]
+var tUser;
+tUser = ['Rajan', true, 23];
+var rgb = [255, 123, 43];
+var newUser = ['rajan', 9879];
+//  newUser.push("raj")
+newUser[1] = 43;
+console.log(newUser);
---
+++ tuple.ts
@@ -0,0 +1,15 @@
+
+
+// const user:(string|number)[]=[1,'hc',3,4]
+let tUser:[string,boolean,number]
+tUser=['Rajan',true,23]
+
+let rgb:[number,number,number]=[255,123,43]
+
+ type User=[string,number]
+ let newUser:User=['rajan',9879]
+//  newUser.push("raj")
+newUser[1]=43
+console.log(newUser)
+
+export {}
