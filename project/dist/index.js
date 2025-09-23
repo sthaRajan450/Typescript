953aa1c72d98e2b0b546ab4bb9a7eb1eb11cce04
sthaRajan450 <stharajan477@gmail.com>
2025-09-23 13:42:45 +0545

    class and access modifiers  in ts

--- project/dist/index.js
+++ project/dist/index.js
@@ -1,5 +1,27 @@
 "use strict";
+// console.log('typescript is here')
+// console.log('typescript is awesome')
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log('typescript is here');
-console.log('typescript is awesome');
+// class User{
+//    public email:string;
+//     name:string;
+//    private readonly city:string=''
+//     constructor(email:string,name:string)
+//     {
+//         this.email=email;
+//         this.name=name;
+//     }
+// }
+class User {
+    email;
+    name;
+    userId;
+    constructor(email, name, userId) {
+        this.email = email;
+        this.name = name;
+        this.userId = userId;
+    }
+}
+const user1 = new User('r@gmail.com', 'rajan', '32');
+console.log(user1);
 //# sourceMappingURL=index.js.map
--- project/src/index.ts
+++ project/src/index.ts
@@ -1,2 +1,23 @@
-console.log('typescript is here')
-console.log('typescript is awesome')
+// console.log('typescript is here')
+// console.log('typescript is awesome')
+
+// class User{
+//    public email:string;
+//     name:string;
+//    private readonly city:string=''
+//     constructor(email:string,name:string)
+//     {
+//         this.email=email;
+//         this.name=name;
+        
+
+//     }
+// }
+
+class User{
+    constructor(public email:string,public name:string,private userId:string){
+    }
+}
+
+const user1=new User('r@gmail.com','rajan','32')
+console.log(user1)
