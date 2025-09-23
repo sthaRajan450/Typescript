a49398b53cac278d22256af88a586302f67043ba
sthaRajan450 <stharajan477@gmail.com>
2025-09-23 13:18:30 +0545

    project setup in ts

---
+++ project/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('typescript is here');
+console.log('typescript is awesome');
+//# sourceMappingURL=index.js.map
